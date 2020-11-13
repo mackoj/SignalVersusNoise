b1385c05087488beddb0b0a324f6e1b58904fcda
Jeffrey Macko <github.jm@macko.fr>
2020-11-14 00:54:24 +0100

    [feat] Sending is now correct

--- TestSVNServer/AppView.swift
+++ TestSVNServer/AppView.swift
@@ -9,23 +9,24 @@ struct AppView: View {
         WithViewStore(store) { viewStore in
             NavigationView {
                 Sidebar(store: self.store.scope(state: \.sidebar, action: AppAction.sidebar))
-                if viewStore.feed == nil && viewStore.timeline == nil {
-                    VStack {
-                        Image(systemName: "trash")
-                        Text("Nothing")
-                    }
-                } else {
-                    IfLetStore(
-                        self.store.scope(state: \.feed, action: AppAction.feed),
-                        then: FeedView.init(store:),
-                        else: Text("No Feed")
-                    )
-                    IfLetStore(
-                        self.store.scope(state: \.timeline, action: AppAction.timeline),
-                        then: TimelineView.init(store:),
-                        else: Text("No Timeline")
-                    )
-                }
+                IfLetStore(
+                    self.store.scope(state: \.feed, action: AppAction.feed),
+                    then: FeedView.init(store:),
+                    else: Text("No Feed")
+                )
+
+//                if viewStore.feed == nil || viewStore.timeline == nil {
+//                    VStack {
+//                        Image(systemName: "trash")
+//                        Text("Nothing")
+//                    }
+//                } else {
+//                    IfLetStore(
+//                        self.store.scope(state: \.timeline, action: AppAction.timeline),
+//                        then: TimelineView.init(store:),
+//                        else: Text("No Timeline")
+//                    )
+//                }
             }
         }
     }
--- TestSVNServer/FeedView.swift
+++ TestSVNServer/FeedView.swift
@@ -7,29 +7,23 @@ import ComposableArchitecture
 
 struct FeedView: View {
     let store : Store<FeedState, FeedAction>
-    
+    let dateFormater = DateFormatter()
     init(store : Store<FeedState, FeedAction>) {
         self.store = store
     }
     
+    
+    
     var body: some View {
         WithViewStore(self.store) { viewStore in
             ForEach(viewStore.rows, id: \.id) { feed in
-                Text("Bobo")
-//                TimelineView(store:
-//                              Store(
-//                                initialState: feed,
-//                                reducer: timelineReducer,
-//                                environment:
-//                              )
-//                )
+                Text(feed.id)
+//                if let sessions = feed.session {
+//                    ForEach(sessions.events) { event in
+//                        Text(dateFormater.string(from: Date(timeIntervalSinceReferenceDate: event.timestamp)))
+//                    }
+//                }
             }
-//            ForEachStore(viewStore.rows) { localStore -> View in
-//                TimelineView(store: localStore)
-//            }
-//            List(self.model.fruits, selection: $selection) { fruit in
-//                FruitRow(fruit: fruit).tag(fruit.id)
-//            }
         }
     }
 }
